e65ab7d74feaf8629a195a009c989410d80d995f
Ben Foxall <benfoxall@gmail.com>
2025-09-18 09:38:11 +0100

    Collection script

---
+++ collect.sh
@@ -0,0 +1,7 @@
+
+curl https://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML > currentTrains.xml
+curl https://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML > stations.xml
+
+git add currentTrains.xml
+git add stations.xml
+
